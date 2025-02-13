d46adcd33b4d69f3c9fbd5b491d056c9560ae89c
zaid7263 <mz726351@gmail.com>
2025-02-13 14:33:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLoveStory() {
+    document.getElementById('loveStory').classList.toggle('hidden');
+}
+function showGallery() {
+    document.getElementById('gallery').classList.toggle('hidden');
+}
+function showSurprise() {
+    document.getElementById('surprise').classList.toggle('hidden');
+}
